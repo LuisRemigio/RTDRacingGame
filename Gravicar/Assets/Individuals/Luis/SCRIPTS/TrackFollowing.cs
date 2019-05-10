1b799231918d38f7ecfbc4bf0a89238a68548095
Luis Remigio <LDRemigio@student.fullsail.edu>
2019-05-10 15:07:00 -0400

    Made minor changes

--- Gravicar/Assets/Individuals/Luis/SCRIPTS/TrackFollowing.cs
+++ Gravicar/Assets/Individuals/Luis/SCRIPTS/TrackFollowing.cs
@@ -7,7 +7,7 @@ public class TrackFollowing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // This is garbage
     }
 
     // Update is called once per frame
