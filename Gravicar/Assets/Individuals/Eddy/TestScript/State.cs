019833dbc9ccb30e66eca0b0632bd783a36086ce
Chenfeng Zhang <akupaqi123@gmail.com>
2019-05-12 12:47:01 -0400

    State machine

---
+++ Gravicar/Assets/Individuals/Eddy/TestScript/State.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using StateStuff;
+
+public class State : State<StateBaseScript>
+{
+    private static State _instance;
+    private State()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+    }
+
+    public static State Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                new State();
+            }
+
+            return _instance;
+        }
+    }
+
+    public override void EnterState(StateBaseScript _owner)
+    {
+        Debug.Log("Entering this State");
+        
+    }
+
+    public override void ExitState(StateBaseScript _owner)
+    {
+        Debug.Log("Exiting this State");
+        
+    }
+
+    public override void UpdateState(StateBaseScript _owner)
+    {
+        
+    }
+}
---
+++ Gravicar/Assets/Individuals/Eddy/TestScript/StateBaseScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+using UnityEngine.AI;
+
+public class StateBaseScript : MonoBehaviour
+{
+
+    public StateMachine<StateBaseScript> stateMachine { get; set; }
+
+    void Awake()
+    {
+        stateMachine = new StateMachine<StateBaseScript>(this);
+        stateMachine.ChangeState(State.Instance);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        stateMachine.Update();
+    }
+}
