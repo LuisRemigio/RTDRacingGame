0edd8b89a7641c9ed5805f575402816067b8eb87
Chenfeng Zhang <akupaqi123@gmail.com>
2019-05-13 14:15:41 -0400

    Upload AI Statemachine

---
+++ Gravicar/Assets/Individuals/Eddy/TestScript/AIStatemachine/AIDriveState.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using StateStuff;
+
+public class AIDriveState : State<AIStateMachine>
+{
+    private static AIDriveState _instance;
+    private AIDriveState()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+    }
+
+    public static AIDriveState Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                new AIDriveState();
+            }
+
+            return _instance;
+        }
+    }
+
+    public override void EnterState(AIStateMachine _owner)
+    {
+        Debug.Log("Entering AIDriveState State");
+        
+    }
+
+    public override void ExitState(AIStateMachine _owner)
+    {
+        Debug.Log("Exiting AIDriveState State");
+
+    }
+
+    public override void UpdateState(AIStateMachine _owner)
+    {
+        _owner.Drive();
+    }
+}
