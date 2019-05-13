8cdd60b79476cbd47ddde4b36865ff2f0a954db7
Luis Remigio <LDRemigio@student.fullsail.edu>
2019-05-13 19:47:36 -0400

    Terrain tracking has been refined and implemented something in case the car flips on a track

--- Gravicar/Assets/Individuals/Luis/SCRIPTS/ArtificialGravity.cs
+++ Gravicar/Assets/Individuals/Luis/SCRIPTS/ArtificialGravity.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class ArtificialGravity : MonoBehaviour
 {
     [SerializeField] float gravitationalForce = 10.0f;
+    [SerializeField] Collider lastCollider;
     // Update is called once per frame
     void Update()
     {
-        gameObject.GetComponent<Rigidbody>().AddForce(-gameObject.transform.up * gameObject.GetComponent<Rigidbody>().mass * gravitationalForce);
+        if (lastCollider == null)
+            gameObject.GetComponent<Rigidbody>().AddForce(-gameObject.transform.up * gameObject.GetComponent<Rigidbody>().mass * gravitationalForce);
+        else
+            gameObject.GetComponent<Rigidbody>().AddForce(-lastCollider.transform.up * gameObject.GetComponent<Rigidbody>().mass * gravitationalForce);
+
+    }
+
+    public void setCollider(Collider c)
+    {
+        lastCollider = c;
     }
 }
