5c8bfe5af4b958c2d2abe8cc25d42d584a4d8c0a
Luis Remigio <LDRemigio@student.fullsail.edu>
2019-05-14 17:58:39 -0400

    Compiling assets into top level folders for impending switch to branching version control

---
+++ Gravicar/Assets/SCRIPTS/VEHICLE/DragSimulation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragSimulation : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float frictionPercentage = .2f;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rb.velocity != Vector3.zero)
+        {
+            rb.AddForce(-rb.velocity * frictionPercentage);
+        }
+    }
+}
