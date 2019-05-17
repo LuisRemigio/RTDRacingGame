6d649ce2f60af0142f684417018f038faf23344e
Luis Remigio <LDRemigio@student.fullsail.edu>
2019-05-17 15:54:33 -0400

    Rear camera works and put sound assets into Unity Project

---
+++ Gravicar/Assets/PROJECT/SCRIPTS/SOUND/scr_SoundManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_SoundManager : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> SFX;
+    [SerializeField] AudioSource soundSource;
+    // Template for sound effects
+    public void PlaySound(int soundIndex)
+    {
+        soundSource.clip = SFX[soundIndex];
+    }
+}
