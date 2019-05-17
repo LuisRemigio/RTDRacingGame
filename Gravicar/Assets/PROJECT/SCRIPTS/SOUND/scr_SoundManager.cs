12c7858ca4584e621141fb962b8001168db58e26
Luis Remigio <LDRemigio@student.fullsail.edu>
2019-05-17 16:47:44 -0400

    Added AudioManager to handle playing sound effects

--- Gravicar/Assets/PROJECT/SCRIPTS/SOUND/scr_SoundManager.cs
+++ Gravicar/Assets/PROJECT/SCRIPTS/SOUND/scr_SoundManager.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class scr_SoundManager : MonoBehaviour
 {
+    AudioSource soundSource;
+    [SerializeField] GameObject audioManager;
     [SerializeField] List<AudioClip> SFX;
-    [SerializeField] AudioSource soundSource;
+    [SerializeField] GameObject mainCamera;
+
     // Template for sound effects
     public void PlaySound(int soundIndex)
     {
+        // Create the sound source
+        soundSource = Instantiate(audioManager, mainCamera.transform).GetComponent<AudioSource>();
+        // Put the desired sound effect on it from a list of sounds
         soundSource.clip = SFX[soundIndex];
+        // Play the sound effect
+        soundSource.Play();
     }
+
 }
